fdcce419d81115b7118d8f994578076299a10383
Aryan Jain <aryanjain29004@gmail.com>
2026-05-18 14:30:28 +0530

    fix: add Vercel URL to CORS config

--- src/main/java/com/audio/transcribe/WebConfig.java
+++ src/main/java/com/audio/transcribe/WebConfig.java
@@ -3,18 +3,19 @@ package com.audio.transcribe;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*")
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedOriginPatterns(
+                        "http://localhost:*",
+                        "https://ai-audio-transcribe-frontend.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-
-
-
     }
 }
